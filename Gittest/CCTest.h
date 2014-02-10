c39ae54c56bb4dedbe3b195d29046f297dee3cb8
Anca Negrean <anca@Radus-MacBook-Pro.local>
2014-02-10 13:31:41 -0500

    added a test class

---
+++ Gittest/CCTest.h
@@ -0,0 +1,13 @@
+//
+//  CCTest.h
+//  Gittest
+//
+//  Created by Anca Negrean on 2/10/14.
+//  Copyright (c) 2014 Radu Negrean. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface CCTest : NSObject
+
+@end
---
+++ Gittest/CCTest.m
@@ -0,0 +1,13 @@
+//
+//  CCTest.m
+//  Gittest
+//
+//  Created by Anca Negrean on 2/10/14.
+//  Copyright (c) 2014 Radu Negrean. All rights reserved.
+//
+
+#import "CCTest.h"
+
+@implementation CCTest
+
+@end
